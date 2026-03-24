7b9986107ad06a88c8589c535f756dac479f1aad
fiash-dev <fiash099rnd@gmail.com>
2026-03-24 22:33:28 +0300

    Вроде робит)

---
+++ src/ru/aston/hometask/Module2/Main.java
@@ -0,0 +1,37 @@
+package ru.aston.hometask.Module2;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        ObjectMapper mapper = new ObjectMapper();
+        List<Student> students = null;
+        try {
+            students = mapper.readValue(new File("src/ru/aston/hometask/Module2/resource/students.json"), new TypeReference<List<Student>>() {
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        // Единый стрим
+        students.stream()
+                .peek(System.out::println)
+                .flatMap(student -> student.getBooks().stream())
+                .distinct()
+                .sorted(Comparator.comparingInt(Book::getPages))
+                .filter(book -> book.getYear() > 2000)
+                .limit(3)
+                .map(Book::getYear)
+                .findFirst()
+                .ifPresentOrElse(
+                        year -> System.out.println("Год выпуска найденной книги: " + year),
+                        () -> System.out.println("Такая книга отсутствует")
+                );
+    }
+}
